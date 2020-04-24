eed589183def4b0392f6870f94f03e29459a25d6
souravtanwar <43500829+souravtanwar@users.noreply.github.com>
2020-04-24 14:59:16 +0530

    Update insertion.cpp

--- insertion.cpp
+++ insertion.cpp
@@ -1,5 +1,5 @@
 #include<iostream>
-#include <bits/stdc++.h> 
+#include <bits/stdc++.h>
 
 using namespace std;
 
@@ -9,22 +9,38 @@ struct Node {
 };
 typedef struct Node node;
 
+int count1(node ***Head)
+{   
+    node* ptr;
+    int count =0;
+    ptr = **Head;
+    while (ptr) {
+       count++;
+       ptr = ptr->next;
+    }
+    cout<<endl;
+    return count;
+
+}
 
 void insert_node_SLL(node **Head, node* newnode, int pos)
 {
-    int k=1;
+    int k=1,c;
     node *p,*q;
     p =*Head;
+    c  = count1(&Head);
+    
     if(pos ==1){
         newnode->next =p;
         *Head = newnode;
     }
     else{
+        if (c+1>=pos){
         while(p !=NULL && k<pos)
         {
             k++;
             q=p;
-            p =p->next; 
+            p =p->next;
         }
         if(p==NULL)
         {
@@ -36,7 +52,13 @@ void insert_node_SLL(node **Head, node* newnode, int pos)
             q->next =newnode;
             newnode ->next =p;
         }
+        }
+        else{
+            cout<<"position does not exist"<<endl;
+        }
     }
+    
+    
 }
 
 void delete_node_SLL(node **Head, int pos)
@@ -45,8 +67,9 @@ void delete_node_SLL(node **Head, int pos)
     int k=1;
     if(!Head)
     {
-        cout<<"List is empty";
+        cout<<"List is empty"<<endl;
     }
+    else{
     p = *Head;
     if(pos==1)
     {
@@ -64,7 +87,7 @@ void delete_node_SLL(node **Head, int pos)
         }
         if(p==NULL)
         {
-            cout<<"Position does not exist";
+            cout<<"Position does not exist"<<endl;
         }
         else
         {
@@ -73,22 +96,24 @@ void delete_node_SLL(node **Head, int pos)
             p = NULL;
         }
     }
-
+    }
 }
 
-void display(node **Head){ 
-   node* ptr;  
-   int count =0; 
+int display(node **Head)
+{
+   node* ptr;
+   int count =0;
    ptr = *Head;
-   while (ptr) { 
+   while (ptr) {
        count++;
        cout<< ptr->data <<" ";
-       ptr = ptr->next; 
-       
-   } 
-   
-} 
-int main() { 
+       ptr = ptr->next;
+    }
+   cout<<endl;
+   return count;
+
+}
+int main() {
     int position, data ,i;
 
     cout<<"\n\t LINKED LIST OPERATIONS";
@@ -106,19 +131,22 @@ int main() {
                 cin>>position;
                 newnode = (node*)malloc(sizeof(node));
                 cout<<"Enter the data : ";
-                cin>>data; 
-                newnode->data = data; 
-                newnode ->next = NULL; 
+                cin>>data;
+                newnode->data = data;
+                newnode ->next = NULL;
                 insert_node_SLL(&Head, newnode,position);
                 break;
-                
+
             case 2:
                 cout<<"Enter the position in which you want to delete a node : ";
                 cin>>position;
                 delete_node_SLL(&Head,position);
                 break;
 
-            case 3:display(&Head);
+            case 3:int  y;
+                    y= display(&Head);
+                    cout<<y;
+                    cout<<endl;
                     break;
             case 4:
                     cout<<"EXIT POINT";
@@ -126,8 +154,9 @@ int main() {
             default:
                    cout<<"\n\t Please Enter a Valid Choice(1/2/3/4)"<<endl;
                     break;
-
         }
     } while (i!=4);
-   return 0; 
-} 
+
+    
+   return 0;
+}
